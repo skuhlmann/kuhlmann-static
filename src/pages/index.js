b8cfafea848f879cbb0577d9c608a19bcce65ebe
skuhlmann <samkuhlmann@gmail.com>
2018-11-13 15:51:29 -0700

    Copy change

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,10 +9,10 @@ const IndexPage = () => (
     <div className="Home__content">
       <h1 className="Home__headline">Sam Kuhlmann</h1>
       <div className="Home__block">
-        <p>I'm a software developer. I like delivering high quality applications solving human and business problems.</p>
+        <p>I build web products. Full stack development and product management for apps and dapps.</p>
       </div>
       <div className="Home__block Home__block--work">
-        <p>Founder and Development at <a href="https://odyssy.io">Odyssy</a></p>
+        <p>Founder/Development @ <a className="Home__block--link" href="https://odyssy.io">Odyssy</a></p>
         <p><a href="https://www.wanderingbloom.com">Joycean</a></p>
         <a className="button" href="mailto:samkuhlmann@gmail.com">Make Contact</a>
       </div>
